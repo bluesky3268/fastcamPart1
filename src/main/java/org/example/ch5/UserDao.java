4fb3e8d019be4e86d3b9ce0eb74959e931c9a150
hbcho <bluesky3268@gmail.com>
2023-02-15 13:08:22 +0900

    JDBC프로그래밍

---
+++ src/main/java/org/example/ch5/UserDao.java
@@ -0,0 +1,73 @@
+package org.example.ch5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.*;
+
+public class UserDao {
+
+
+    public Connection getConnection() {
+        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=1";
+        String id = "sa";
+        String password = "";
+
+        try{
+            Class.forName("org.h2.Driver");
+            return DriverManager.getConnection(url, id, password);
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    public void create(User user) throws SQLException{
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try{
+            conn = getConnection();
+            String sql = "INSERT INTO USERS VALUES(?, ?, ?, ?)";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+
+            pstmt.executeUpdate();
+        }finally {
+            if(pstmt != null) pstmt.close();
+            if(conn != null) conn.close();
+        }
+    }
+
+    public User findById(String userId) throws SQLException {
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        User user = null;
+        try{
+            conn = getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, userId);
+
+            rs = pstmt.executeQuery();
+
+            if(rs.next())
+                user = new User(rs.getString("userId"),
+                                rs.getString("password"),
+                                rs.getString("name"),
+                                rs.getString("email")
+                                );
+        }finally {
+            if(rs != null) rs.close();
+            if(pstmt != null) pstmt.close();
+            if(conn != null) conn.close();
+        }
+
+        return user;
+    }
+}
